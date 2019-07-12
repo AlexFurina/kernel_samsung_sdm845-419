a46ba1f033f0b6908a3971dbf6b974b01041e2d6
Angelo G. Del Regno <kholk11@gmail.com>
2019-07-12 12:42:44 +0200

    clk: qcom: aop-qmp: Add support for v1 AOP clocks
    Due to HW limitations on v1, the qdss_ao clock was not supported
    by the clock driver on AOP.
    
    This is needed for SDM845.

--- drivers/clk/qcom/clk-aop-qmp.c
+++ drivers/clk/qcom/clk-aop-qmp.c
@@ -222,6 +222,15 @@ static struct clk_hw *aop_qmp_clk_hws[] = {
 	[QDSS_AO_CLK] = &qdss_ao_qmp_clk.hw,
 };
 
+/*
+ * Due to HW limitations on v1, the qdss_ao clock was not supported by the clock
+ * driver on AOP.
+ */
+static void aop_qmp_fixup_v1(void)
+{
+	aop_qmp_clk_hws[QDSS_AO_CLK] = NULL;
+}
+
 static int qmp_update_client(struct clk_hw *hw, struct device *dev,
 		struct mbox_chan **mbox)
 {
@@ -280,6 +289,9 @@ static int aop_qmp_clk_probe(struct platform_device *pdev)
 
 	clk_data->clk_num = num_clks;
 
+	if (of_device_is_compatible(pdev->dev.of_node, "qcom,aop-qmp-clk-v1"))
+		aop_qmp_fixup_v1();
+
 	for (i = 1; i < num_clks; i++) {
 		if (!aop_qmp_clk_hws[i])
 			continue;
@@ -328,6 +340,7 @@ fail:
 
 static const struct of_device_id aop_qmp_clk_of_match[] = {
 	{ .compatible = "qcom,aop-qmp-clk", },
+	{ .compatible = "qcom,aop-qmp-clk-v1" },
 	{}
 };
 
